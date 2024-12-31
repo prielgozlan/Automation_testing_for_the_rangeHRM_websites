e6d6d0167e8e6dea3f5de8ab2037eac2d8c01c1b
priel gozlan <prielgozlan@gmail.com>
2024-12-31 22:29:48 +0200

    fix page main with junit

--- src/main/java/ProjectRangeHRM/pages/mainPage.java
+++ src/main/java/ProjectRangeHRM/pages/mainPage.java
@@ -12,7 +12,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 import java.util.Objects;
-import java.util.Scanner;
 
 import static java.lang.Thread.sleep;
 
@@ -39,69 +38,83 @@ public class mainPage {
         PageFactory.initElements(driver , this);
     }
 
-    public void checkListOfThePages() throws InterruptedException {
+    public String checkListOfThePages() throws InterruptedException {
 
         for(int i = 0 ; i < listPage.size() ; i++){
             if(!Objects.equals(listPage.get(i).getText(), "Maintenance")){
                 listPage.get(i).click();
                 if(Objects.equals(listPage.get(i).getText(), namePage.getText())){
                     System.out.println(listPage.get(i).getText()+": "+ "The page does match the title in the page.");
+
                 }
                 else {
                     System.out.println(listPage.get(i).getText()+": " + "The page does not match the title in the page.");
+                    return listPage.get(i).getText()+": " + "The page does not match the title in the page.";
                 }
             }
+
         }
+        return "The page does match the title in the page.";
+
 
     }
-    public void searchPage() throws InterruptedException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Search for the page that you want:");
-        String namePage = scanner.nextLine();
-        enterNamePage.sendKeys(namePage);
+    public String searchPage() throws InterruptedException {
+        enterNamePage.sendKeys("pim");
         if(!Objects.equals(findPage.get(1).getText(), "")){
             System.out.println("result for search : "+findPage.get(1).getText());
-
+            return "result for search : "+findPage.get(1).getText();
         }
         else{
             System.out.println("not found nothing");
-
+            return "not found nothing";
         }
     }
-    public void logout() throws InterruptedException {
+    public String logout() throws InterruptedException {
         profileMenu.click();
         logoutButton.click();
         String currentUrl = driver.getCurrentUrl();
         if (currentUrl.contains("/auth/login")){
             System.out.println("Logged out successfully");
+            return "Logged out successfully";
         }
         else {
             System.out.println("Did not log out successfully");
+            return "Did not log out successfully";
         }
     }
-    public void Responsive() throws InterruptedException {
+    public String Responsive() throws InterruptedException {
         PageLoadTimePage PageLoadTimePage = new PageLoadTimePage(driver);
         long startTimeDesktop = System.currentTimeMillis();
         driver.manage().window().setSize(new Dimension(1920, 1080));
-        PageLoadTimePage.getPerformance(startTimeDesktop);
-
+        long loadTimeDesktop = PageLoadTimePage.getPerformance(startTimeDesktop);
+        if (loadTimeDesktop > 3000) {
+            return "Page load time is too high in Desktop";
+        }
         long startTimeTablet = System.currentTimeMillis();
         driver.manage().window().setSize(new Dimension(768, 1024));
-        PageLoadTimePage.getPerformance(startTimeTablet);
-
+        long loadTimeTablet  = PageLoadTimePage.getPerformance(startTimeTablet);
+        if (loadTimeTablet > 3000) {
+            return "Page load time is too high in Tablet";
+        }
         long startTimeMobile = System.currentTimeMillis();
         driver.manage().window().setSize(new Dimension(375, 667));
-        PageLoadTimePage.getPerformance(startTimeMobile);
+        long loadTimeMobile = PageLoadTimePage.getPerformance(startTimeMobile);
+        if (loadTimeMobile > 3000) {
+            return "Page load time is too high in Mobile";
+        }
+        return "everything pass ok";
     }
-    public void linkValidation(){
+    public String linkValidation(){
         List<WebElement> links = driver.findElements(By.tagName("a"));
         for (WebElement link : links) {
             String url = link.getAttribute("href");
             int statusCode = checkLinkStatus(url);
             if (statusCode != 200){
                 System.out.println("Error number : "+ statusCode +" "+url);
+                return "Error number : "+ statusCode +" "+url;
             }
         }
+        return "all links pass ok";
     }
      public static int checkLinkStatus(String url) {
             try {
