433878dd972dfb4469d7120f8b98f3c4796045d5
priel gozlan <prielgozlan@gmail.com>
2025-01-12 22:23:19 +0200

    Adding test report for each page

--- src/test/java/ProjectRangeHRM/seleniumBase.java
+++ src/test/java/ProjectRangeHRM/seleniumBase.java
@@ -7,14 +7,27 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class seleniumBase {
 
     public static ChromeDriver driver;
+    protected static ExtentReports extent;
+
     @BeforeAll
     public static void seleniumInit() throws InterruptedException {
         System.out.println("Starting...");
+
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String reportPath = System.getProperty("user.dir") + "/src/test/resources/TestReport_" + timestamp + ".html";
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
+        extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
+
+        // אתחול של ה-ChromeDriver
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");    //fix for chrome version 111
         options.addArguments("--start-maximized");
@@ -28,17 +41,8 @@ public class seleniumBase {
     @AfterAll
     public static void seleniumClose(){
         System.out.println("close test ...");
+        // שמירה של הדוח (לא מוחק אותו, אלא מוסיף אליו)
         extent.flush();
         driver.close();
-    }
-    protected static ExtentReports extent;
-
-    static {
-        if (extent == null) {
-            String myPath = System.getProperty("user.dir") + "/src/test/resources/combinedTestReport.html";
-            ExtentSparkReporter sparkReporter = new ExtentSparkReporter(myPath);
-            extent = new ExtentReports();
-            extent.attachReporter(sparkReporter);
-        }
     }
 }
